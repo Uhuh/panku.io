3140b5b77ac5e37a72eb540a29c6b4d80a0b7f5c
Dylan Warren <dylanwarren19@gmail.com>
2023-11-04 01:05:05 -0500

    update: card to have load indicator and load images lazily

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,22 +1,34 @@
-import { Component, Input } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common';
+import { Component, HostBinding, Input } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass],
 })
 export class CardComponent {
-  @Input({ required: true }) assetName!: string;
+  @Input() asset?: string;
+  @Input() link?: string;
   @Input() twitterName?: string;
 
-  get asset() {
-    return `assets/${this.assetName}`;
+  loaded = false;
+
+  @HostBinding('style.min-height') get loading() {
+    return this.loaded ? '' : '200px';
+  }
+
+  get url() {
+    return this.asset ? `assets/${this.asset}` : this.link;
   }
 
   get twitter() {
     return `https://twitter.com/${this.twitterName}`;
+  }
+
+  // LoadEvent
+  onLoad(e: any) {
+    this.loaded = e.target.complete;
   }
 }
